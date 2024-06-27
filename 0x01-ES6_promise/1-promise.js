6b6c6dabfa4a50d16c5e2f23703417678744d023
Voxold <bilalajaha@gmail.com>
2024-06-27 13:42:58 +0100

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,27 +1,14 @@
-
-let p = new Promise(getFullResponseFromAPI(resolve, reject)
-	rt default function (boolean) {
-		  return new Promise((resolve, reject) => {
-			      const object = {
-				            status: 200,
-				            body: 'Success',
-				          };
-
-			      if (boolean === true) {
-				            resolve(object);
-				          } else {
-						        reject(Error('The fake API is not working currently'));
-						      }
-			    });
-	}
- {
-	 const object={
-		 status: 200,
-		 body: 'success',
-	 }
-	 if (true){
-	 	resolve(object)
-	 }else{
-	 reject(Error('The fake API is not working currently'));
-	 }
- });
+export default function (boolean) {
+	return new Promise((resolve, reject) => {
+	  const object = {
+		status: 200,
+		body: 'Success',
+	  };
+  
+	  if (boolean === true) {
+		resolve(object);
+	  } else {
+		reject(Error('The fake API is not working currently'));
+	  }
+	});
+  }
