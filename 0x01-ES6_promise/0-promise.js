b44c7c2a49087bb4e1e49abf2ee2b58ece4f9dd3
Voxold <bilalajaha@gmail.com>
2024-06-27 13:44:52 +0100

    Return new Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
-function getResponseFromAPI(){
-	return new Promise((resolve) => {
-		resolve();
-	});
-}
-export default getResponseFromAPI;
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => [resolve, reject]);
+  }
