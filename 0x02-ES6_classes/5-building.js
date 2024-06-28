a81d888bf05e6cd4b4ac49e11284fc4c73dc03d5
Voxold <bilalajaha@gmail.com>
2024-06-28 15:40:25 +0100

    Task of Buildings

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+    constructor(sqft = 0) {
+      if (this.constructor !== Building
+            && typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+  
+      this._sqft = sqft;
+    }
+  
+    // Getter for sqft
+    get sqft() {
+      return this._sqft;
+    }
+  }
